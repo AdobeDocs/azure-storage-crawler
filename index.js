9664e54f526380647b9c17b5de25adc587bd6f2b
timkim <timk@adobe.com>
2023-07-10 13:41:00 -0700

    Exclude aem ref materials

--- index.js
+++ index.js
@@ -3,6 +3,21 @@ require("dotenv").config();
 const fs = require('fs').promises;
 const { BlobServiceClient } = require("@azure/storage-blob");
 
+function excluder(filename) {
+    if(filename.includes('reference-materials/6-4')){
+        return false;
+    } else if( filename.includes('reference-materials/6-5')) {
+        return false;
+    } else if( filename.includes('reference-materials/cloud-service')) {
+        return false;
+    } else if( filename.includes('reference-materials/spec')) {
+        return false;
+    } else if( filename.includes('404')) {
+        return false;
+    } else {
+        return true;
+    }
+}
 
 async function main() {
     const connectionString = process.env.CONNECTION_STRING || "";
@@ -15,11 +30,14 @@ async function main() {
 
     let data ='';
     for await (const blob of containerService.listBlobsFlat()){
-
-        if(blob.name.endsWith('.html') || blob.name.endsWith('.htm')) {
-            console.log(blob.name);
-            data += blob.name + '\n';
+        // exclude aem reference materials and 404 pages
+        if(excluder(blob.name)) {
+            if(blob.name.endsWith('.html') || blob.name.endsWith('.htm')) {
+                console.log(blob.name);
+                data += blob.name + '\n';
+            }
         }
+
     }
     await fs.writeFile('site_map.txt', data); // need to be in an async function
 
