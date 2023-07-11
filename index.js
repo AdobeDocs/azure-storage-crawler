bdab84d0b02dfe29715ca2f819dd911074cd62ca
Dmitry Matiouchenko <dmitrymatio@gmail.com>
2023-07-11 15:03:06 -0700

    feat: now generates xml sitemap instead of txt file

--- index.js
+++ index.js
@@ -1,46 +1,62 @@
 // Load the .env file if it exists
 require("dotenv").config();
-const fs = require('fs').promises;
 const { BlobServiceClient } = require("@azure/storage-blob");
 
+const { createWriteStream } = require("fs");
+const { SitemapStream } = require("sitemap");
+
 function excluder(filename) {
-    if(filename.includes('reference-materials/6-4')){
-        return false;
-    } else if( filename.includes('reference-materials/6-5')) {
-        return false;
-    } else if( filename.includes('reference-materials/cloud-service')) {
-        return false;
-    } else if( filename.includes('reference-materials/spec')) {
-        return false;
-    } else if( filename.includes('404')) {
-        return false;
-    } else {
-        return true;
-    }
+  if (filename.includes("reference-materials/6-4")) {
+    return false;
+  } else if (filename.includes("reference-materials/6-5")) {
+    return false;
+  } else if (filename.includes("reference-materials/cloud-service")) {
+    return false;
+  } else if (filename.includes("reference-materials/spec")) {
+    return false;
+  } else if (filename.includes("404")) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
 async function main() {
-    const connectionString = process.env.CONNECTION_STRING || "";
-    const containerName =  '$web' ;
+  const connectionString = process.env.CONNECTION_STRING || "";
+  const containerName = "$web";
 
-    console.log('Running')
-    
-    const blobServiceClient = await BlobServiceClient.fromConnectionString(connectionString);
-    const containerService = blobServiceClient.getContainerClient(containerName);
+  console.log("Running");
 
-    let data ='';
-    for await (const blob of containerService.listBlobsFlat()){
-        // exclude aem reference materials and 404 pages
-        if(excluder(blob.name)) {
-            if(blob.name.endsWith('.html') || blob.name.endsWith('.htm')) {
-                console.log(blob.name);
-                data += blob.name + '\n';
-            }
-        }
+  const blobServiceClient = await BlobServiceClient.fromConnectionString(
+    connectionString
+  );
+  const containerService = blobServiceClient.getContainerClient(containerName);
 
+  let links = [];
+  for await (const blob of containerService.listBlobsFlat()) {
+    // exclude aem reference materials and 404 pages
+    if (excluder(blob.name)) {
+      if (blob.name.endsWith(".html") || blob.name.endsWith(".htm")) {
+        // console.log(blob.name);
+        links.push(
+          blob.name.replace("index.html", "").replace("index.htm", "")
+        );
+      }
     }
-    await fs.writeFile('site_map.txt', data); // need to be in an async function
+  }
+  // Create a stream to write to
+  const sitemap = new SitemapStream({
+    hostname: "https://developer.adobe.com/",
+  });
+  const writeStream = createWriteStream("./sitemap.xml");
+  sitemap.pipe(writeStream);
 
+  links.forEach((link) => {
+    const linkObj = { url: link, changefreq: "monthly", priority: 0.7 };
+    console.log(linkObj);
+    sitemap.write(linkObj);
+  });
+  sitemap.end();
 }
 
 main();
